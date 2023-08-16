a8e145e5ba4034e1795a9dac3a89f17afa1499a8
sebas2rus <sebas2rus@gmail.com>
2023-08-16 10:28:04 -0500

    Search update

--- src/main.js
+++ src/main.js
@@ -13,14 +13,43 @@ export function eventHandler(e) {
 }
 form.addEventListener('submit', eventHandler)
 
+const formi = document.getElementById('searchFormi')
+export function eventHandleri(e) {
+    const username = document.getElementById('searchi').value
+    validateData(username)
+    e.preventDefault();
+    formi.reset()
+}
+formi.addEventListener('submit', eventHandleri)
+
+
+const formii = document.getElementById('searchFormii')
+console.log(formii);
+export function eventHandlerii(e) {
+    console.log(e);
+    const username = document.getElementById('searchii').value
+    console.log(username);
+    validateData(username)
+    e.preventDefault();
+    formii.reset()
+}
+formii.addEventListener('submit', eventHandlerii)
+
+
 
 // Defines what content to render depending on the information
 export async function validateData(username) {
     const data = await writeData(username)
+    console.log(data);
     if (data && data?.message == 'Not Found') {
-        document.location.replace('http://127.0.0.1:5500/pages/notFound.html')
-    }
-    if (data && data?.login != '') {
-        document.location.replace('http://127.0.0.1:5500/pages/found.html')
+        const searchView = document.getElementById('searchView').style.display = 'none';
+        const foundView = document.getElementById('foundView').style.display = 'none';
+        const notFoundView = document.getElementById('notFoundView').style.display = 'block';
+
+    } else if (data && data?.login != '') {
+        const searchView = document.getElementById('searchView').style.display = 'none';
+        const notFoundView = document.getElementById('notFoundView').style.display = 'none';
+        const foundView = document.getElementById('foundView').style.display = 'block';
+
     }
 }
